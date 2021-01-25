c1be288416f3b896dd52297df22a349a2a839b57
re3turn <re3turn@gmail.com>
2021-01-26 03:55:17 +0900

    Fix tests for fishtape v3

--- tests/dot.fish
+++ tests/dot.fish
@@ -5,19 +5,19 @@
     )
 
 @test "Not set DOT_SCR" \
-    "Please 'set -U DOT_SCR <dot_source_ource_clone_repository>' 1" = (
+    "1" = (
         set -l PREV_DOT_SCR $DOT_SCR
         set -e DOT_SCR
-        dot
+        dot > /dev/null 2>&1
         echo $status
         set DOT_SCR $PREV_DOT_SCR
     )
 
 @test "Not install dot" \
-    "dot is not installed. 1" = (
+    "1" = (
         set -l PREV_DOT_SCR $DOT_SCR
         set DOT_SCR /not_exist_path.....
-        dot
+        dot > /dev/null 2>&1
         echo $status
         set DOT_SCR $PREV_DOT_SCR
     )
